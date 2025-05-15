8f890e4662d1adc5a8e0fcd88082c9a4cadef6f4
Vivek Vidyasagaran <vivek.v.sagar@gmail.com>
2025-05-15 13:12:09 -0400

    export static page from next

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/vivek-website",
+  assetPrefix: "/vivek-website",
 };
 
 export default nextConfig;
