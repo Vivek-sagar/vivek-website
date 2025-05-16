8934c5aa2120495dc2e4fbd486e1f721ce45fdae
Vivek Vidyasagaran <vivek.v.sagar@gmail.com>
2025-05-15 21:32:45 -0400

    more config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   basePath: "/vivek-website",
   assetPrefix: "/vivek-website/",
+  images: {
+    unoptimized: true,
+    path: "/vivek-website",
+  },
   /* config options here */
 };
 
